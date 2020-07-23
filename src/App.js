6153b568b16d5941d39a7897ea17b97072bd3c31
Nicholas Leask <leask.nicholas@gmail.com>
2020-07-23 14:56:11 -0400

    added nav, home, about, & react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar';
+import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import About from './Components/About';
 import { Route, Switch } from 'react-router-dom';
@@ -8,7 +8,7 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavBar/>
       <header className="App-header">
         <Switch>
           <Route path="/" component={Home} exact />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
-function Navbar(){
+function NavBar(){
     return (
-        <div className='navbar'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About Me</Link>
-            {/* <Link to='/'>Projects</Link> */}
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                
+                </Nav>
+                <Nav>
+                    <NavDropdown title="Projects" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="https://videosync.co/" target="blank">Video Sync</NavDropdown.Item>
+                        <NavDropdown.Item href="https://cnn.com/" target="blank">CNN</NavDropdown.Item>
+                        <NavDropdown.Item href="https://google.com/" target="blank">Google</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
-export default Navbar;
+export default NavBar;
